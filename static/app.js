305be36f965afd790c5899bc2db1cc3d54c5c6ec
Eugene Doan <doan.eugene@gmail.com>
2016-05-19 21:16:29 -0400

    Used icon for form button. Moved (new) loading animation to button. Added custom fonts. Added CSS tooltips for substitutions.

--- static/app.js
+++ static/app.js
@@ -1,7 +1,13 @@
 $(function () {
     $("#xkcd-form").submit(function (event) {
         event.preventDefault();
-        $("#content").html("<div class='loader'>Loading...</div>");
+        var loader = $("<span>"
+                       + "<i class='fa fa-refresh fa-spin'></i>"
+                       + "<span class='sr-only'>Loading...</span>"
+                       + "</span>");
+        var button = $("#xkcd-form button");
+        button.children().hide();
+        button.append(loader);
         $.get({
             url: "/xkcdify",
             data: {
@@ -12,10 +18,21 @@ $(function () {
                 $("#content").html(data);
             },
             error: function (xhr, status, error) {
-                $("#content").html("<h1>Whoops! Something went wrong...</h1>"
-                                   + "<p>Unable to complete your request.</p>");
+                var h1 = $("<h1>").text("Whoops! Something went wrong...");
+                var p = $("<p>");
+                if (status == "timeout") {
+                    p.text("The request timed out.");
+                }
+                else {
+                    p.text("Unable to complete your request");
+                }
+                $("#content").html("");
+                $("#content").append(h1).append(p);
             },
             timeout: 5000
+        }).complete(function () {
+            loader.remove();
+            button.children().show();
         });
     });
 });
